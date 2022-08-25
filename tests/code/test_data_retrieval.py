cc46198ca763e10237e8abf831ea5b539a83cfd2
kelvinou01 <kelvin.ou.jin.bin@gmail.com>
2022-08-25 08:53:54 +0800

    Remove TestGetArticles
    TestGetArticles tests if the API works, instead of our own code.

--- tests/code/test_data_retrieval.py
+++ tests/code/test_data_retrieval.py
@@ -5,31 +5,6 @@ from unittest.mock import patch
 
 from ml.data_retrieval import fetch_articles_from_news_api, get_articles
 
-class TestFetchArticlesFromNewsApi:
-    @classmethod
-    def setup_class(cls):
-        cls.default_term = 'American economy'
-        cls.articles = fetch_articles_from_news_api(cls.default_term, config.SOURCES[:3])
-
-    @classmethod
-    def teardown_class(cls):
-        pass
-
-    def test_fetch(self): 
-        for article in self.articles: 
-            assert article.keys() == {'source', 'author', 'title', 'description', 
-                                      'url', 'urlToImage', 'publishedAt', 'content'}
-
-    def test_sources(self): 
-        nytimes_only = ['nytimes.com']
-        articles = fetch_articles_from_news_api(self.default_term, nytimes_only)
-        for article in articles: 
-            assert article['source']['name'] == 'New York Times'
-            assert article['url'].startswith('https://www.nytimes.com/')
-
-    def test_length(self):
-         assert len(self.articles) > 0
-
 
 class TestGetArticles: 
     @classmethod
