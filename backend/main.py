26237d97a6513324ab0bde42ae1c9c028fa9a1ff
kelvinou01 <kelvin.ou.jin.bin@gmail.com>
2022-09-09 14:33:49 +0800

    Add rate limiting for main endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,16 @@
 
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+from slowapi.errors import RateLimitExceeded
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
 
 from backend.models import Timeline
 from config import config
 from ml.data_retrieval import Storage, fetch_articles_from_news_api
 from ml.pipelines import sliding_window_optics_pipeline
 
-
+limiter = Limiter(key_func=get_remote_address)
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
@@ -15,11 +18,14 @@ app.add_middleware(
     allow_methods=['*'],
     allow_headers=['*']
 )
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 storage = Storage()
 
 @app.get("/timeline", response_model=Timeline)
-async def get_timeline(q: str):
+@limiter.limit('1/second')
+async def get_timeline(request: Request, q: str):
 
     timeline_file_name = f'{q.replace(" ", "_")}.json'
     timeline_is_cached, timeline = storage.get_timeline(timeline_file_name)
