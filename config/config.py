fb085529de392b9995bfb008537c8c80f7695f93
kelvinou01 <kelvin.ou.jin.bin@gmail.com>
2022-09-18 14:10:43 +0800

    Update ARTICLES_TO_REMOVE

--- config/config.py
+++ config/config.py
@@ -41,9 +41,11 @@ SOURCES = ['economist.com', 'apnews.com','reuters.com','theguardian.com',
 
 ARTICLES_TO_REMOVE = (
     'Russia-Ukraine war: what we know on day',  # The Guardian
+    'Russia-Ukraine war latest: what we know on day',  # The Guardian
     'Timeline: Week',  # Al Jazeera
     'Russia-Ukraine war: List of key events',  # Al Jazeera
     'Russia-Ukraine war: What happened today',  # NPR
+    'Russia-Ukraine war: A weekly recap and look ahead',  # NPR
 )
 
 # Ref: https://github.com/dipanjanS/text-analytics-with-python/blob/master/New-Second-Edition/Ch05%20-%20Text%20Classification/contractions.py
