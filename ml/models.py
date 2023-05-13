95d0ae35584d41801cae5035e7c354db441137d4
kelvinou01 <kelvin.ou.jin.bin@gmail.com>
2023-05-13 10:03:55 +0800

    Changed embeddings from SpaCy to BERT

--- ml/models.py
+++ ml/models.py
@@ -1,24 +1,27 @@
 import numpy as np
-import spacy
 from .utils import partition_into_clusters
+import sentence_transformers
+import torch
+import gc
 
-nlp = spacy.load('en_core_web_lg', 
-                 exclude=['ner', 'tok2vec', 'tagger', 'parser', 'senter', 
-                          'textcat', 'attribute_ruler', 'lemmatizer'])
-
-def embed_text_column(df):
+device = "cuda" if torch.cuda.is_available() else "cpu"
+sent_transformer = sentence_transformers.SentenceTransformer('all-MiniLM-L6-v2', device=device)
+sent_transformer.max_seq_length = 512
+def embed_articles(df):
     '''
-    Embeds the `text` column of a DataFrame into an `embeddings` column.
+    Generates article embeddings `embeddings` column.
     '''
+    title_embeddings = sent_transformer.encode(list(df.title))
+    body_embeddings = sent_transformer.encode(list(df.body))
+    embeddings = (0.5 * title_embeddings) + (0.5 * body_embeddings)
+    
+    torch.cuda.empty_cache()
+    gc.collect()
 
-    docs = list(nlp.pipe(df.text))
-    embeddings = [doc.vector for doc in docs]
-    df_with_embeddings = df.copy()
-    df_with_embeddings['embeddings'] = embeddings
-    return df_with_embeddings
+    return list(embeddings)
 
 
-def cluster_each_window(model, sliding_windows, keep_raw_text=False, keep_embeddings=False):
+def cluster_each_window(model, sliding_windows, keep_embeddings=False):
     '''
     Given a model and a list of DataFrames, where sliding_windows[w] is a DataFrame of articles,  
     Returns a list of list of DataFrames, where clusters_for_each_window[w][c]
@@ -32,8 +35,6 @@ def cluster_each_window(model, sliding_windows, keep_raw_text=False, keep_embedd
         
         if not keep_embeddings:
             window = window.drop(columns=['embeddings'])
-        if not keep_raw_text:
-            window = window.drop(columns=['text'])
 
         clusters = partition_into_clusters(window, model)
         clusters_for_each_window.append(clusters)
