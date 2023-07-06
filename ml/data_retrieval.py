0172a44396d8ea7fc46f0e68cf11d88f93fb5a88
Kelvin Ou <kelvin.ou.jin.bin@gmail.com>
2023-07-06 21:26:50 +0800

    Switch to fastapi-cache2

--- ml/data_retrieval.py
+++ ml/data_retrieval.py
@@ -1,89 +1,12 @@
 
 import grequests
-import json
 import logging
-import os
-import pandas as pd
 import validators
-from google.cloud import storage 
-from google.api_core.exceptions import NotFound
-from google.auth.exceptions import DefaultCredentialsError
-from tempfile import TemporaryDirectory
-from typing import List, Tuple
+from typing import List
 from urllib import parse
 
 from config import config
-from backend.models import Article, Timeline
-
-class Storage:
-    
-    def __init__(self):
-        try:
-            storage_client = storage.Client()
-        except DefaultCredentialsError:
-            message = 'Unable to connect to cloud storage. ' \
-                + 'Check the GOOGLE_APPLICATION_CREDENTIALS environment variable.'
-            logging.debug(message)
-            raise ValueError(message)
-        self.bucket = storage_client.bucket(config.GOOGLE_CLOUD_BUCKET_NAME)
-        
-        self.article_folder_name = config.get_articles_folder_name()
-        self.timeline_folder_name = config.get_timeline_folder_name()
-    
-    def get_articles(self, file_name):
-        assert file_name.endswith('.csv')
-        
-        cloud_file_path = f'{self.article_folder_name}/{file_name}'
-        blob = self.bucket.blob(cloud_file_path)
-        
-        with TemporaryDirectory() as temp_dir:
-            temp_file_path = f'{temp_dir}/{file_name}'
-            with open(temp_file_path, 'w'):
-                try: 
-                    blob.download_to_filename(temp_file_path)
-                    return True, pd.read_csv(temp_file_path)
-                except NotFound:
-                    return False, None
-
-    def save_articles(self, articles, file_name):
-        assert file_name.endswith('.csv')
-
-        cloud_file_path = f'{self.article_folder_name}/{file_name}'
-        blob = self.bucket.blob(cloud_file_path)
-
-        with TemporaryDirectory() as temp_dir:
-            temp_file_path = f'{temp_dir}/{file_name}'
-            with open(temp_file_path, 'w'):
-                articles.to_csv(temp_file_path, index=False)
-                blob.upload_from_filename(temp_file_path)
-
-    def get_timeline(self, file_name: str) -> Tuple[bool, Timeline]:
-        assert file_name.endswith('.json')
-
-        cloud_file_path = f'{self.timeline_folder_name}/{file_name}'
-        blob = self.bucket.blob(cloud_file_path)
-        
-        with TemporaryDirectory() as temp_dir:
-            temp_file_path = f'{temp_dir}/{file_name}'
-            try:
-                blob.download_to_filename(temp_file_path)
-            except NotFound:
-                return False, None
-            with open(temp_file_path) as file:
-                return True, json.load(file)
-
-    def save_timeline(self, timeline: Timeline, file_name: str):
-        assert file_name.endswith('.json')
-
-        cloud_file_path = f'{self.timeline_folder_name}/{file_name}'
-        blob = self.bucket.blob(cloud_file_path) 
-        
-        with TemporaryDirectory() as temp_dir:
-            temp_file_path = f'{temp_dir}/{file_name}'
-            os.makedirs(os.path.dirname(temp_file_path), exist_ok=True)
-            with open(temp_file_path, 'w') as fp:
-                json.dump(timeline, fp, default=str)
-            blob.upload_from_filename(temp_file_path)
+from backend.models import Article
 
 
 def remove_invalid_articles(articles):
